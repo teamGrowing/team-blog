a51eb839f561778b4c0d319da0e2ecafdb8a3e22
Yeonju Jo <yjj0287@gmail.com>
2024-02-24 01:09:54 +0900

    chore: pathPrefix 변경

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const blogConfig = require("./blog-config")
 const { title, description, author, siteUrl } = blogConfig
 
 module.exports = {
-  pathPrefix: "/gatsby-starter-hoodie",
+  pathPrefix: "/team-blog",
   siteMetadata: {
     title,
     description,
