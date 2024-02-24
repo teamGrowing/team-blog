f5796419d32d6b10f1d5e2e7f94c3c666962a1a7
Yeonju Jo <yjj0287@gmail.com>
2024-02-24 21:17:52 +0900

    fix: site url 수정

--- blog-config.js
+++ blog-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "Team Growing",
   description: "안녕하세요. 그로잉 FE팀 블로그입니다.",
   author: "Growing",
-  siteUrl: "https://teamGrowing.github.io/growing-team-blog/",
+  siteUrl: "https://teamGrowing.github.io/team-blog",
   links: {
     github: "https://github.com/teamGrowing",
   },
